8dca142a09a8ede30ecc873b4453b92af31092bd
sebastiansliwinski <s.sliwinski@storware.eu>
2022-01-30 18:58:08 +0100

    support for multiple rules

--- src/components/input/Text.tsx
+++ src/components/input/Text.tsx
@@ -1,11 +1,36 @@
-import React from 'react';
+
+
+import React, {useEffect, useState} from 'react';
 import { InputText } from 'primereact/inputtext';
 
-const Text = ({ ...props }) => {
+const Text = ({ inputValue, ...props }) => {
+  let value;
+  let setValue;
+  [value, setValue] = useState(inputValue);
+
+  const setFieldValueAndEmitChangeEvent = (value) => {
+    setValue(value);
+    if (props.change) {
+      props.change({
+        value,
+      });
+    }
+  };
+
+  useEffect(() => {
+    setValue(inputValue);
+  }, [inputValue]);
+
   return (
     <div>
       {!!props.label && <label>{props.label}</label>}
-      <InputText {...props} />
+      <InputText
+        {...props}
+        value={value}
+        onChange={(e: any) => {
+          setFieldValueAndEmitChangeEvent(e.target.value);
+        }}
+      />
     </div>
   );
 };
--- src/pages/policies/rules/RulesContainer.tsx
+++ src/pages/policies/rules/RulesContainer.tsx
@@ -1,17 +1,24 @@
 import React, {useEffect, useState} from 'react';
-import {backupDestinationsService} from '../../../services/backup-destinations-service';
 import {schedulesService} from '../../../services/schedules-service';
-import {BackupDestination} from '../../../model/backup-destination/backup-destination';
-import Select from '../../../components/input/Select';
 import Text from '../../../components/input/Text';
-import {InputConvert} from '../../../components/input/InputConvert';
 import {ListBox} from 'primereact/listbox';
 import {ToggleButton} from 'primereact/togglebutton';
 import {Button} from 'primereact/button';
+import Select from '../../../components/input/Select';
+import {BackupDestination} from '../../../model/backup-destination/backup-destination';
+import {NameAndGuid} from '../../../model/dto/nameAndGuid';
+import {BackupDestinationRetentionFactory} from '../../../model/backup-destination/backup-destination-retention-factory';
 
-export const RulesContainer = ({rule, backupDestinations, updateFilteredBackupDestinations}) => {
-  console.log(rule)
+export const RulesContainer = ({rule, filteredBackupDestinations, updateFilteredBackupDestinations, removeRule}) => {
+  const [backupDestinationType, setBackupDestinationType] = useState(null);
+  const [primaryBackupDestinationOptions, setPrimaryBackupDestinationOptions] = useState(filteredBackupDestinations);
+  const [secondaryBackupDestinationOptions, setSecondaryBackupDestinationOptions] = useState(filteredBackupDestinations);
+  const [backupDestinationsCopy] = useState(filteredBackupDestinations);
   const [schedules, setSchedules] = useState([]);
+  const [factor] = useState(24 * 60 * 60 * 1000);
+
+  // form
+  const [keepLastBackupWhenSourceStillExists, setKeepLastBackupWhenSourceStillExists] = useState(false);
 
   useEffect(() => {
     schedulesService.getAllTypeSchedules('VM_BACKUP').then((result) => {
@@ -19,20 +26,108 @@ export const RulesContainer = ({rule, backupDestinations, updateFilteredBackupDe
     });
   }, []);
 
-  const updateBackupDestination = (event) => {
-    console.log(event)
-    // updateFilteredBackupDestinations('Data from child');
-    // event.preventDefault();
+  useEffect(() => {
+    setBackupDestinationOptions();
+  }, [filteredBackupDestinations]);
+
+  useEffect(() => {
+    updateBackupDestination();
+    setKeepLastBackupWhenSourceStillExists(
+      rule.ruleBackupDestinations.primaryBackupDestination.backupRetentionSettings.keepLastBackupWhenSourceStillExists)
+  }, [rule]);
+
+  const updateBackupDestination = (event?) => {
+    setBackupRetentionSettings();
+    updateFilteredBackupDestinations();
+
+    if (event) {
+      event.preventDefault();
+    }
+  }
+
+  const setBackupDestinationOptions = () => {
+    if (rule.ruleBackupDestinations.primaryBackupDestination?.backupDestination?.guid) {
+      setPrimaryBackupDestinationOptions([
+        rule.ruleBackupDestinations.primaryBackupDestination.backupDestination,
+        ...filteredBackupDestinations,
+      ]);
+
+      setSecondaryBackupDestinationOptions([...filteredBackupDestinations])
+    } else {
+      setPrimaryBackupDestinationOptions([
+        ...filteredBackupDestinations,
+      ]);
+      setSecondaryBackupDestinationOptions([
+        ...filteredBackupDestinations,
+      ]);
+    }
+
+    if (rule.ruleBackupDestinations.secondaryBackupDestination?.backupDestination?.guid) {
+      setSecondaryBackupDestinationOptions([
+        rule.ruleBackupDestinations.secondaryBackupDestination.backupDestination,
+        ...filteredBackupDestinations,
+      ])
+    }
+  }
+
+  const setBackupRetentionSettings = () => {
+    if (!rule.ruleBackupDestinations.primaryBackupDestination.backupRetentionSettings.retentionKeepLastNFull) {
+      setTypeAndRetention(rule.ruleBackupDestinations.primaryBackupDestination.backupDestination);
+    } else {
+      setType(rule.ruleBackupDestinations.primaryBackupDestination.backupDestination);
+    }
+  }
+
+  const setTypeAndRetention = (selectedBackupDestination: BackupDestination | NameAndGuid) => {
+    setType(selectedBackupDestination);
+    if (!backupDestinationType) {
+      return;
+    }
+
+    rule.ruleBackupDestinations.primaryBackupDestination = {
+      ...rule.ruleBackupDestinations.primaryBackupDestination,
+      backupRetentionSettings: new BackupDestinationRetentionFactory[backupDestinationType](),
+    };
+    rule.ruleBackupDestinations.secondaryBackupDestination = {
+      ...rule.ruleBackupDestinations.secondaryBackupDestination,
+      backupRetentionSettings: new BackupDestinationRetentionFactory[backupDestinationType](),
+    };
+  }
+
+  const setType = (selectedBackupDestination: BackupDestination | NameAndGuid) => {
+    if (!backupDestinationsCopy.length) {
+      return;
+    }
+    setBackupDestinationType(backupDestinationsCopy.find(
+      ({guid}) => guid === selectedBackupDestination.guid
+    )?.type?.name);
+  }
+
+  const emitRemoveRule = (event) => {
+    removeRule();
+    event.preventDefault();
+  }
+
+  const setRetentionSettingsValue = (key, value) => {
+    rule.ruleBackupDestinations.primaryBackupDestination.backupRetentionSettings[key] = value * factor;
+    rule.ruleBackupDestinations.secondaryBackupDestination.backupRetentionSettings[key] = value * factor;
+  }
+
+  const getRetentionSettingsValue = (key) => {
+    return rule.ruleBackupDestinations.primaryBackupDestination.backupRetentionSettings[key] / factor
   }
 
   return (
     <>
+      {rule.position !== 0 &&
       <div className='text-right'>
-        <Button label='Remove rule' />
+        <Button type='button' label='Remove rule' onClick={emitRemoveRule}/>
       </div>
+      }
 
       <Text
-        value={rule.name}
+        inputValue={rule.name}
+        change={(e) => rule.name = e.value}
         label='Name'
       />
 
@@ -42,17 +137,24 @@ export const RulesContainer = ({rule, backupDestinations, updateFilteredBackupDe
             label='Select Primary Backup Destination'
             optionLabel='name'
             dataKey='guid'
+            required={true}
             value={rule.ruleBackupDestinations.primaryBackupDestination.backupDestination}
-            options={backupDestinations}
-            onChange={updateBackupDestination}
+            options={primaryBackupDestinationOptions}
+            onChange={(e) => {
+              rule.ruleBackupDestinations.primaryBackupDestination.backupDestination = e.value;
+              updateBackupDestination(e);
+            }}
           />
         </div>
 
         <div style={{'width': '48%'}} className='mt-2'>
           <Select
             value={rule.ruleBackupDestinations.secondaryBackupDestination.backupDestination}
-            onChange={updateBackupDestination}
-            options={backupDestinations}
+            onChange={(e) => {
+              rule.ruleBackupDestinations.secondaryBackupDestination.backupDestination = e.value
+              updateBackupDestination(e);
+            }}
+            options={secondaryBackupDestinationOptions}
             optionLabel='name'
             multiple
             dataKey='guid'
@@ -63,18 +165,21 @@ export const RulesContainer = ({rule, backupDestinations, updateFilteredBackupDe
 
       <div className='d-flex justify-content-between'>
         <div style={{'width': '48%'}} className='mt-2'>
-          <InputConvert
-            factor={24 * 60 * 60 * 1000}
-            value={rule.ruleBackupDestinations.primaryBackupDestination.backupRetentionSettings.retentionKeepFullNewerThan}
+          <Text
             label='Retention (Full) - number of days to keep'
-          />
+            inputValue={getRetentionSettingsValue('retentionKeepFullNewerThan')}
+            change={(e) => {
+              setRetentionSettingsValue('retentionKeepFullNewerThan', e.value)
+            }}/>
         </div>
 
         <div style={{'width': '48%'}} className='mt-2'>
-          <InputConvert
-            factor={24 * 60 * 60 * 1000}
-            value={rule.ruleBackupDestinations.primaryBackupDestination.backupRetentionSettings.retentionKeepIncrementalNewerThan}
+          <Text
             label='Retention (Inc.) - number of days to keep'
+            inputValue={getRetentionSettingsValue('retentionKeepIncrementalNewerThan')}
+            change={(e) => {
+              setRetentionSettingsValue('retentionKeepIncrementalNewerThan', e.value)
+            }}
           />
         </div>
       </div>
@@ -82,22 +187,35 @@ export const RulesContainer = ({rule, backupDestinations, updateFilteredBackupDe
       <div className='d-flex justify-content-between'>
         <div style={{'width': '48%'}} className='mt-2'>
           <Text
-            value={rule.ruleBackupDestinations.primaryBackupDestination.backupRetentionSettings.retentionKeepLastNFull}
+            inputValue={rule.ruleBackupDestinations.primaryBackupDestination.backupRetentionSettings.retentionKeepLastNFull}
             label='Retention (Full) - number of versions to keep'
+            change={(e) => {
+              rule.ruleBackupDestinations.primaryBackupDestination.backupRetentionSettings.retentionKeepLastNFull = e.value
+              rule.ruleBackupDestinations.secondaryBackupDestination.backupRetentionSettings.retentionKeepLastNFull = e.value
+            }}
           />
         </div>
 
         <div style={{'width': '48%'}} className='mt-2'>
           <Text
-            value={rule.ruleBackupDestinations.primaryBackupDestination.backupRetentionSettings.retentionKeepLastNIncremental}
+            inputValue={rule.ruleBackupDestinations.primaryBackupDestination.backupRetentionSettings.retentionKeepLastNIncremental}
             label='Retention (Inc.) - number of versions to keep'
+            change={(e) => {
+              rule.ruleBackupDestinations.primaryBackupDestination.backupRetentionSettings.retentionKeepLastNIncremental = e.value
+              rule.ruleBackupDestinations.secondaryBackupDestination.backupRetentionSettings.retentionKeepLastNIncremental = e.value
+            }}
           />
         </div>
       </div>
 
       <div className='my-2'>
         <ToggleButton
-          checked={rule.ruleBackupDestinations.primaryBackupDestination.backupRetentionSettings.retentionKeepLastNIncremental}
+          checked={keepLastBackupWhenSourceStillExists}
+          onChange={(e) => {
+            setKeepLastBackupWhenSourceStillExists(e.value)
+            rule.ruleBackupDestinations.primaryBackupDestination.backupRetentionSettings.keepLastBackupWhenSourceStillExists = e.value
+            rule.ruleBackupDestinations.secondaryBackupDestination.backupRetentionSettings.keepLastBackupWhenSourceStillExists = e.value
+          }}
         />
         <label className='ml-2'>Keep last backup when source still exists</label>
       </div>
@@ -108,6 +226,9 @@ export const RulesContainer = ({rule, backupDestinations, updateFilteredBackupDe
         options={schedules} optionLabel='name'
         multiple
         dataKey='guid'
+        onChange={(e) => {
+          rule.ruleBackupDestinations.schedules = e.value
+        }}
       />
     </>
   )
