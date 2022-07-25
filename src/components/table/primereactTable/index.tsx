0597156d7309dd26e8180a9921cffa00af324010
sebastiansliwinski <s.sliwinski@storware.eu>
2022-07-25 14:12:27 +0200

    changed pagination template
    (cherry picked from commit f2869d7371f09939307886a450626684c6ca9b7f)

--- src/components/table/primereactTable/index.tsx
+++ src/components/table/primereactTable/index.tsx
@@ -1,9 +1,37 @@
 import React from 'react';
 import { DataTable } from 'primereact/datatable';
+import {Dropdown} from 'primereact/dropdown';
+import {PaginatorTemplate} from 'primereact/paginator';
+
+const Paginator = {
+  layout: 'RowsPerPageDropdown CurrentPageReport PrevPageLink NextPageLink',
+  'RowsPerPageDropdown': (options) => {
+    const dropdownOptions = [
+      { label: 5, value: 5 },
+      { label: 10, value: 10 },
+      { label: 20, value: 20 },
+    ];
+
+    return (
+      <React.Fragment>
+        <span className="mx-1" style={{ color: 'var(--text-color)', userSelect: 'none' }}>Items per page: </span>
+        <Dropdown value={options.value} options={dropdownOptions} onChange={options.onChange} />
+      </React.Fragment>
+    );
+  },
+  'CurrentPageReport': (options) => {
+    return (
+      <span style={{ color: 'var(--text-color)', userSelect: 'none', width: '120px', textAlign: 'center' }}>
+                    {options.first} - {options.last} of {options.totalRecords}
+                </span>
+    )
+  }
+} as PaginatorTemplate;
 
 const Table = ({ children, ...props }) => (
   <div className={'c-table'}>
-    <DataTable paginator rows={10} rowsPerPageOptions={[5, 10, 20]} {...props}>
+    <DataTable paginator paginatorTemplate={Paginator} rows={10} {...props}
+               paginatorClassName="justify-content-end" className="mt-6">
       {children}
     </DataTable>
   </div>
