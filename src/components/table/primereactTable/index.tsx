e98bf3b269c65b2ab59ef3259af6c4eba71df83a
Przemyslaw Krakowski <p.krakowski@storware.eu>
2022-08-24 11:14:02 +0200

    [SVP-8089] Feature: No sorting in tables in the whole B&R

--- src/components/table/primereactTable/index.tsx
+++ src/components/table/primereactTable/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { DataTable } from 'primereact/datatable';
-import {Dropdown} from 'primereact/dropdown';
-import {PaginatorTemplate} from 'primereact/paginator';
+import { Dropdown } from 'primereact/dropdown';
+import { PaginatorTemplate } from 'primereact/paginator';
 
 const Paginator = {
   layout: 'RowsPerPageDropdown CurrentPageReport PrevPageLink NextPageLink',
-  'RowsPerPageDropdown': (options) => {
+  RowsPerPageDropdown: (options) => {
     const dropdownOptions = [
       { label: 5, value: 5 },
       { label: 10, value: 10 },
@@ -14,24 +14,48 @@ const Paginator = {
 
     return (
       <React.Fragment>
-        <span className="mx-1" style={{ color: 'var(--text-color)', userSelect: 'none' }}>Items per page: </span>
-        <Dropdown value={options.value} options={dropdownOptions} onChange={options.onChange} />
+        <span
+          className="mx-1"
+          style={{ color: 'var(--text-color)', userSelect: 'none' }}
+        >
+          Items per page:{' '}
+        </span>
+        <Dropdown
+          value={options.value}
+          options={dropdownOptions}
+          onChange={options.onChange}
+        />
       </React.Fragment>
     );
   },
-  'CurrentPageReport': (options) => {
+  CurrentPageReport: (options) => {
     return (
-      <span style={{ color: 'var(--text-color)', userSelect: 'none', width: '120px', textAlign: 'center' }}>
-                    {options.first} - {options.last} of {options.totalRecords}
-                </span>
-    )
-  }
+      <span
+        style={{
+          color: 'var(--text-color)',
+          userSelect: 'none',
+          width: '120px',
+          textAlign: 'center',
+        }}
+      >
+        {options.first} - {options.last} of {options.totalRecords}
+      </span>
+    );
+  },
 } as PaginatorTemplate;
 
 const Table = ({ children, ...props }) => (
   <div className={'c-table'}>
-    <DataTable paginator paginatorTemplate={Paginator} rows={10} {...props}
-               paginatorClassName="justify-content-end" className="mt-6">
+    <DataTable
+      paginator
+      paginatorTemplate={Paginator}
+      rows={10}
+      {...props}
+      paginatorClassName="justify-content-end"
+      className="mt-6"
+      removableSort
+      sortMode={'multiple'}
+    >
       {children}
     </DataTable>
   </div>
