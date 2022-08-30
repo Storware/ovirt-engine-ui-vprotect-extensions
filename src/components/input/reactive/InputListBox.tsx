ba004d44ab17aa217d5155f08cbc605b7abe1e6d
krakowskiprzem <111743636+krakowskiprzem@users.noreply.github.com>
2022-08-30 14:10:12 +0200

    [SVP-8078] Feature:No information about Hypervisor Manager and UUID while creating the policy

--- src/components/input/reactive/InputListBox.tsx
+++ src/components/input/reactive/InputListBox.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import { ListBox } from 'primereact/listbox';
 
+const listBoxElement = ({ name, uuid }) => (
+  <div>
+    <p>{name}</p>
+    <p style={{ fontSize: '0.8em', color: 'grey' }}>{uuid}</p>
+  </div>
+);
+
 const InputListBox = ({ field, ...props }) => (
   <div className="pt-3">
     {!!props.label && <label>{props.label}</label>}
-    <ListBox {...field} {...props} listStyle={{ maxHeight: '250px' }} />
+    <ListBox
+      {...field}
+      {...props}
+      listStyle={{ maxHeight: '250px' }}
+      itemTemplate={listBoxElement}
+    />
   </div>
 );
 
