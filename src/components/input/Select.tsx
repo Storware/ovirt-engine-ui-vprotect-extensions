8ce18526d6ec81fac9713bea264ea28976bf09fe
Tomasz Lipczynski <t.lipczynski@storware.eu>
2022-09-01 13:44:48 +0200

    Fix: Select Backup Destination field has default value.

--- src/components/input/Select.tsx
+++ src/components/input/Select.tsx
@@ -1,20 +1,24 @@
 import React, { useEffect } from 'react';
-import { Dropdown, DropdownProps } from 'primereact/dropdown';
+import {
+  Dropdown,
+  DropdownChangeParams,
+  DropdownProps,
+} from 'primereact/dropdown';
 
 interface Props extends DropdownProps {
   value: unknown;
   label: string;
+  isRequired?: boolean;
 }
 
 const Select = ({ value, ...props }: Props) => {
   useEffect(() => {
-    if (!props.options[0] || !props.required || value) {
+    if (!props.options[0] || !props.isRequired || value) {
       return;
     }
 
     const optionalValue = props.options[0];
-    // @ts-ignore
-    props.onChange({ value: optionalValue });
+    props.onChange({ value: optionalValue } as any);
   }, [props.options, value]);
 
   return (
