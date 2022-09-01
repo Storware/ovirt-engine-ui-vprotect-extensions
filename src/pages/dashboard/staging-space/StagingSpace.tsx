a191fe465002be4a3af81c15b75d4a66aef1e2d5
Przemyslaw Krakowski <p.krakowski@storware.eu>
2022-09-01 15:49:24 +0200

    [SVP-9212] Fix:The backup destination list in dashboard is ugly

--- src/pages/dashboard/staging-space/StagingSpace.tsx
+++ src/pages/dashboard/staging-space/StagingSpace.tsx
@@ -9,43 +9,34 @@ import {
 import { Filesize } from '../../../components/convert/Filesize';
 import { Accordion } from 'primereact/accordion';
 
-export const StagingSpace = ({ stagingSpace }) => {
-  return (
-    <div className={'col-md-6'}>
-      <div className="card-pf">
-        <div>
-          <div className={'card-pf-heading'}>
-            <div>
-              <h5>Staging Space</h5>
-            </div>
-            <div>
-              <h6 className={'card-pf-title'}>STAGING UTILIZATION PER NODE</h6>
-            </div>
-          </div>
-
-          {stagingSpace.length > 0 &&
-            stagingSpace.map((el) => {
-              const usedSpace = el.totalSpace - el.availableSpace;
-              const percentage = (usedSpace / el.totalSpace) * 100;
-              return (
-                <BorderBox key={el.node.name} heading={el.node.name}>
-                  <div key={el.node.name + 'Progress'} className={'w-100'}>
-                    <div className={'d-flex flex-row justify-content-between'}>
-                      <span className={'color-black'}>
-                        {percentage.toFixed(2)}%
-                      </span>
-                      <span className={'color-black'}>
-                        <Filesize bytes={usedSpace} /> /{' '}
-                        <Filesize bytes={el.totalSpace} />
-                      </span>
-                    </div>
-                    <Progress value={percentage} />
-                  </div>
-                </BorderBox>
-              );
-            })}
-        </div>
+export const StagingSpace = ({ stagingSpace }) => (
+  <div className="card-pf">
+    <div>
+      <div className={'card-pf-heading'}>
+        <h6 className={'card-pf-title'}>STAGING UTILIZATION PER NODE</h6>
       </div>
+
+      {stagingSpace.length > 0 &&
+        stagingSpace.map((el) => {
+          const usedSpace = el.totalSpace - el.availableSpace;
+          const percentage = (usedSpace / el.totalSpace) * 100;
+          return (
+            <BorderBox key={el.node.name} heading={el.node.name}>
+              <div key={el.node.name + 'Progress'} className={'w-100'}>
+                <div className={'d-flex flex-row justify-content-between'}>
+                  <span className={'color-black'}>
+                    {percentage.toFixed(2)}%
+                  </span>
+                  <span className={'color-black'}>
+                    <Filesize bytes={usedSpace} /> /{' '}
+                    <Filesize bytes={el.totalSpace} />
+                  </span>
+                </div>
+                <Progress value={percentage} />
+              </div>
+            </BorderBox>
+          );
+        })}
     </div>
-  );
-};
+  </div>
+);
