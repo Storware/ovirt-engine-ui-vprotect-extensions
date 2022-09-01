35dd31227f9e0a71de481476028ec5a068d26325
Tomasz Lipczynski <t.lipczynski@storware.eu>
2022-09-01 15:43:08 +0200

    [SVP-9513] Fix: Correct length and position of tab's ink in policies.

--- src/pages/policies-and-schedules/ListContainer.tsx
+++ src/pages/policies-and-schedules/ListContainer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Link,
   Switch,
@@ -9,43 +9,70 @@ import {
 } from 'react-router-dom';
 import SchedulesList from 'pages/schedules/SchedulesList';
 import PoliciesList from 'pages/policies/PoliciesList';
+import { TabInk } from '../../model';
 
 const items = [
   { label: 'Policies', path: 'policies' },
   { label: 'Schedules', path: 'schedules' },
 ];
 
-const inkStyle = {
-  policies: { width: '105px', left: '0px' },
-  schedules: { width: '122px', left: '105px' },
-};
-
 export const ListContainer = () => {
   const match = useRouteMatch();
   const location = useLocation();
   const pathParts = location.pathname.split('/');
-  const path = pathParts[pathParts.length - 1];
+  const pathEl = pathParts[pathParts.length - 1];
   const pathWithoutTab = location.pathname.split('/').slice(0, -1).join('/');
+  const [inkStyle, setInkStyle] = useState<TabInk[]>([]);
+
+  useEffect(() => {
+    const list = document.querySelectorAll('.tabmenuitem-getwidth');
+    list.forEach(({ id, clientWidth }) =>
+      setInkStyle((prevState) => [
+        ...prevState,
+        {
+          path: id,
+          width: clientWidth.toString() + 'px',
+          left:
+            prevState.length > 0
+              ? prevState
+                  .reduce(
+                    (prev, curr) => prev + +curr.width.replace('px', ''),
+                    0,
+                  )
+                  .toString() + 'px'
+              : '0px',
+        },
+      ]),
+    );
+  }, []);
+
+  useEffect(() => {
+    console.log(inkStyle);
+  }, [inkStyle]);
 
   return (
     <div>
       <div className="p-tabmenu p-component">
         <ul className="p-tabmenu-nav p-reset">
-          {items.map((el) => (
-            <Link to={`${pathWithoutTab}/${el.path}`} key={el.path}>
+          {items.map(({ path, label }) => (
+            <Link to={`${pathWithoutTab}/${path}`} key={path}>
               <li
-                className={`p-tabmenuitem ${
-                  (path === el.path && 'p-highlight') || ''
+                className={`p-tabmenuitem tabmenuitem-getwidth ${
+                  (pathEl === path && 'p-highlight') || ''
                 } `}
-                key={el.path}
+                id={path}
+                key={path}
               >
                 <a className="p-menuitem-link">
-                  <span className="p-menuitem-text">{el.label}</span>
+                  <span className="p-menuitem-text">{label}</span>
                 </a>
               </li>
             </Link>
           ))}
-          <li className="p-tabmenu-ink-bar" style={inkStyle[path]} />
+          <li
+            className="p-tabmenu-ink-bar"
+            style={inkStyle.find((item) => item.path === pathEl)}
+          />
         </ul>
       </div>
 
