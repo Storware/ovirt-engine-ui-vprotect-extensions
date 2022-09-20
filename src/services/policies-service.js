ef148b619b1cd8e86c45a9f56fe475e6d8b349ea
Przemyslaw Krakowski <p.krakowski@storware.eu>
2022-09-20 15:20:58 +0200

    [SVP-9603] Feature: Add pagination to the policy list

--- src/services/policies-service.js
+++ src/services/policies-service.js
@@ -12,9 +12,14 @@ class PoliciesService {
     {name: 'ASSIGN_AND_REMOVE', description: 'Assign and remove'},
   ];
 
-  async getPolicies(type) {
-    const res = await vprotectApiService.get(`/policies/${type}?extended=true`);
-    return getElementsWithoutProjectUuidInName(res);
+  async getPoliciesPage(type, params) {
+    const res = await vprotectApiService.get(`/policies/${type}`, {
+      params,
+      headers: {},
+      paginate: true,
+    })
+
+    return {body: getElementsWithoutProjectUuidInName(res.body), totalCount: res.totalCount}
   }
 
   async getPolicy(type, guid) {
