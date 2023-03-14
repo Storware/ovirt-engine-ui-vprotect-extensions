e88cfc38b5ca45ace0fcd444fc878e1fbc099c76
tlipczynski <111348522+tlipczynski@users.noreply.github.com>
2023-03-14 15:03:09 +0100

    [SVP-12036] Fix: saving Schedules goes back 2 pages (#396)

--- src/pages/schedules/SchedulesList.tsx
+++ src/pages/schedules/SchedulesList.tsx
@@ -11,7 +11,7 @@ import { Column } from 'primereact/column';
 import { booleanTemplate } from 'components/table/templates';
 import Table from 'components/table/primereactTable';
 import { Button } from 'primereact/button';
-import {userHasPrivilege} from "utils/privileges";
+import { userHasPrivilege } from 'utils/privileges';
 
 const typeMap = {
   'vm-backup': 'VM_BACKUP',
